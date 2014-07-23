6f5f793fab6617f0f26b6f7214e78e965e7c3535
vasi@amvtek <vasi@amvtek>
2014-07-23 18:20:20 +0300

    add numpydoc style docstring

--- src/geohealpix.pyx
+++ src/geohealpix.pyx
@@ -55,17 +55,75 @@ cdef extern from "healpix.h":
         double get_aperture()
 
 def earth_dist(lat0, lon0, lat1, lon1):
-    "return distance in meters in between (lat0,lon0) and (lat1,lon1)"
+    """
+    earth_dist(lat0, lon0, lat1, lon1)
+
+    Compute the distance between two geographical points (lat, lon),
+    by taking into account that Earth surface is not flat.
+
+    Parameters
+    ----------
+    lat0 : float
+        Latitude of the first geographical point.
+    lon0 : float
+        Longitude of the first geographical point.
+    lat1 : float
+        Latitude of the second geographical point.
+    lon1 : float
+        Longitude of the second geographical point.
+
+    Returns
+    -------
+    distance : float
+        Distance in meters in between (lat0,lon0) and (lat1,lon1)
+    """
 
     return earth_dist_deg(lat0, lon0, lat1, lon1)
 
 def bearing(lat0, lon0, lat1, lon1):
-    "return bearing in degree from (lat0,lon0) to (lat1,lon1)"
+    """
+    bearing(lat0, lon0, lat1, lon1):
+
+    Computes baring in degree between two geographical points (lat, lon).
+
+    Parameters
+    ----------
+    lat0 : float
+        Latitude of the first geographical point.
+    lon0 : float
+        Longitude of the first geographical point.
+    lat1 : float
+        Latitude of the second geographical point.
+    lon1 : float
+        Longitude of the second geographical point.
+
+    Returns
+    -------
+    bearing : float
+        Bearing in degree from (lat0,lon0) to (lat1,lon1)
+    """
 
     return bearing_deg(lat0, lon0, lat1, lon1)
 
 def point_at_dist(lat0, lon0, dist, bearing):
-    "return (lat,lon) of point at dist and bearing from (lat0,lon0)"
+    """
+    point_at_dist(lat0, lon0, dist, bearing)
+
+    Parameters
+    ----------
+    lat0 : float
+        Latitude of the geographical point.
+    lon0 : float
+        Longitude of the geographical point.
+    dist : float
+        Distance from the origin point
+    bearing : float
+        Bearing in degree from the origin point.
+
+    Returns
+    -------
+    (lat,lon) of point at dist and bearing from (lat0,lon0)
+    """
 
     cdef double ptlat, ptlon
     point_at_dist_and_bearing_from_ref(lat0,lon0,dist,bearing,&ptlat,&ptlon)
@@ -139,7 +197,15 @@ cdef class GeoGrid(object):
     sphere using equiaera <diamond> shaped cells (called pixels in Healpix)
     The main purpose of this class is to let us index location of moving
     objects without the need to apply local latitude corrections to evaluate
-    distances...
+    distances.
+
+    Attributes
+    ----------
+    grid_order : integer
+        Order of the grid.
+    is_nested : boolean
+        True if grid is NEST, False if grid is RING
+
     """
 
     cdef HealpixGridHR *_grid
@@ -166,20 +232,36 @@ cdef class GeoGrid(object):
         
     def get_code(self,lat,lon):
         """
-        return numerical pixel code for (lat,lon) location
-        
-        @lat : location latitude in decimal degree
-        @lon : location longitude in decimal degree
+        Parameters
+        ----------
+        lat : float
+            location latitude in decimal degree
+        lon : float
+            location longitude in decimal degree
+
+        Returns
+        -------
+        code : integer
+            numerical pixel code for (lat,lon) location
+
         """
         
         return self._grid.get_code(lat,lon)
 
     def get_fcode(self, lat, lon):
         """
-        return formatted pixel code for (lat,lon) location
-        
-        @lat : location latitude in decimal degree
-        @lon : location longitude in decimal degree
+
+        Parameters
+        ----------
+        lat : float
+            location latitude in decimal degree
+        lon : float
+            location longitude in decimal degree
+
+        Returns
+        -------
+        fcode : string
+            formatted pixel code for (lat,lon) location
         """
 
         cdef char[31] scode # max size for rawcode is 29 + 1 + 1...
@@ -188,12 +270,19 @@ cdef class GeoGrid(object):
 
     def list_codes_in_disc(self,lat,lon,radius):
         """
-        return list of numerical pixels code for diamonds
-        at distance less than radius of (lat,lon) center
+        Parameters
+        ----------
+        lat : float
+            center latitude in decimal degree
+        lon : float
+            center longitude in decimal degree
+        radius : float
+            distance in 'earth' radians
 
-        @lat : center latitude in decimal degree
-        @lon : center longitude in decimal degree
-        @radius : distance in 'earth' radians
+        Returns
+        -------
+        codes : list of floats
+            List of numerical pixels code for diamonds at distance less than radius of (lat,lon) center
         """
 
         cdef vector[int64_t] rv = vector[int64_t]()
@@ -203,12 +292,19 @@ cdef class GeoGrid(object):
 
     def list_fcodes_in_disc(self,lat,lon,radius):
         """
-        return list of formatted pixels code for diamonds 
-        at distance less than radius of (lat,lon) center
+        Parameters
+        ----------
+        lat : float
+            center latitude in decimal degree
+        lon : float
+            center longitude in decimal degree
+        radius : float
+            distance in 'earth' radians
 
-        @lat : center latitude in decimal degree
-        @lon : center longitude in decimal degree
-        @radius : distance in 'earth' radians
+        Returns
+        -------
+        fcodes : list of strings
+            List of formatted pixels code for diamonds at distance less than radius of (lat,lon) center
         """
   
         cdef vector[string] rv = vector[string]()
@@ -217,7 +313,12 @@ cdef class GeoGrid(object):
         return rv
 
     def get_scheme(self):
-        "return code scheme as 'RING' or 'NEST'"
+        """
+        Returns
+        -------
+        scheme: 'RING' or 'NEST'
+            code scheme as 'RING' or 'NEST'
+        """
 
         if self._scheme:
             return 'NEST'
@@ -225,19 +326,34 @@ cdef class GeoGrid(object):
     scheme = property(get_scheme)
 
     def get_order(self):
-        "return order"
+        """
+        Returns
+        -------
+        order: integer
+        """
 
         return self._order
     order = property(get_order)
 
     def get_npix(self):
-        "return Npix()"
+        """
+        Returns
+        -------
+        npix : integer
+            Returns the number of pixels.
+        """
 
         return self._grid.Npix()
     npix = property(get_npix)
 
     def get_aperture(self):
-        "return pixel grid aperture"
+        """
+        Returns
+        -------
+        aperture : float
+            Maximum radian aperture ('radian diameter...') that can be covered by a single grid pixel
+
+        """
 
         return self._grid.get_aperture()
     aperture = property(get_aperture)
@@ -258,6 +374,14 @@ cdef class GeoGrid(object):
         del self._grid
 
 class SearchEngine(object):
+    """
+    Provides a way to receive a list of fcodes that will fully cover a map definded by
+    two geographical points representing the SW and NE corners of the map.
+
+    Attributes
+    ----------
+    grids : list of GeoGrid objects
+    """
 
     def __init__(self, *grids):
 
@@ -279,6 +403,25 @@ class SearchEngine(object):
         return (lat, lon, radius)
 
     def list_fcode_tiling_bbox(self, lat0, lon0, lat1, lon1):
+        """
+        list_fcode_tiling_bbox(self, lat0, lon0, lat1, lon1)
+
+        Parameters
+        ----------
+        lat0 : float
+            Latitude of the first geographical point.
+        lon0 : float
+            Longitude of the first geographical point.
+        lat1 : float
+            Latitude of the second geographical point.
+        lon1 : float
+            Longitude of the second geographical point.
+
+        Returns
+        -------
+        fcodes : list of strings
+            Fcodes that cover map between SW(lat0,lon0) and NE(lat1,lon1)
+        """
 
         # minimal search disk to cover rectangular bbox 
         lat, lon, radius = self._get_disk_covering_bbox(lat0, lon0, lat1, lon1)
